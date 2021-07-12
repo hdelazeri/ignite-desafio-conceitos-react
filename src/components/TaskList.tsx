77b70a32768aec156d630dad22955aff40bbf06e
Henrique Delazeri <personal@hdelazeri.dev>
2021-07-12 11:24:14 -0300

    Solving the challenges
    Creating the functions that handle creating, toggling completeness and deleting tasks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,37 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if (newTaskTitle.trim() === '') {
+      return
+    }
+
+    const nweTask: Task = {
+      id: Math.floor(Math.random() * 100000),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks([...tasks, nweTask])
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    setTasks(tasks.map(task => {
+      if (task.id === id) {
+        return {...task, isComplete: !task.isComplete}
+      } else {
+        return task
+      }
+    }))
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    setTasks(tasks.filter(task => task.id !== id))
   }
 
   return (
